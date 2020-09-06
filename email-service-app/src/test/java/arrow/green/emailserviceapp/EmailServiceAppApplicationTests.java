3c92d49a075a46b141ba8521f2eff146fb28913a
nakulGoyal19 <nakul19goyal@gmail.com>
2020-09-06 18:32:06 +0530

    Base setup for email-service-app

---
+++ email-service-app/src/test/java/arrow/green/emailserviceapp/EmailServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package arrow.green.emailserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
