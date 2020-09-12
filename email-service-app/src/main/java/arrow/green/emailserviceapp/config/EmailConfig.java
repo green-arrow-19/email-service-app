7cbd9d777626b9c3cc93be004ebcfaa8cde000b9
nakulGoyal19 <nakul19goyal@gmail.com>
2020-09-12 21:09:21 +0530

    Change in email config props

--- email-service-app/src/main/java/arrow/green/emailserviceapp/config/EmailConfig.java
+++ email-service-app/src/main/java/arrow/green/emailserviceapp/config/EmailConfig.java
@@ -1,47 +1,52 @@
 package arrow.green.emailserviceapp.config;
 
+import java.util.Map;
 import java.util.Properties;
+import javax.annotation.PostConstruct;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author nakulgoyal
  *         06/09/20
  **/
 
+@Slf4j
 @Data
 @Configuration
+@ConfigurationProperties(prefix = "email.service")
 public class EmailConfig {
     
-    @Value("${email.service.props.username}")
+    private Map<String, String> props;
+    
+    @Setter(AccessLevel.NONE)
     private String username;
-    @Value("${email.service.props.password}")
-    private String password;
-    @Value("${email.service.props.smtp_auth}")
-    private String smtp_auth;
-    @Value("${email.service.props.starttls_enabled}")
-    private String starttls_enabled;
-    @Value("${email.service.props.host}")
-    private String host;
-    @Value("${email.service.props.port}")
-    private String port;
     
     public Session getSession() {
         Properties props = new Properties();
-        props.put("mail.smtp.auth", smtp_auth);
-        props.put("mail.smtp.starttls.enable", starttls_enabled);
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", port);
-        
+        props.put("mail.smtp.auth", props.getProperty("smtp_auth"));
+        props.put("mail.smtp.starttls.enable", props.getProperty("starttls_enabled"));
+        props.put("mail.smtp.host", props.getProperty("host"));
+        props.put("mail.smtp.port", props.getProperty("port"));
         return Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
+                return new PasswordAuthentication(props.getProperty("username"), props.getProperty("password"));
             }
         });
+    }
+    
+    @PostConstruct
+    public void init() {
+        this.username = props.get("username");
+        log.info("Email config props : {}", props);
     }
     
 }
